c0f9c999bf51790ea6797ffa3085c350a4fbf578
matiasrantanen <matias.rantanen@hotmail.fi>
2018-10-17 16:07:38 +0300

    Update app_server/models/db.js

--- app_server/models/db.js
+++ app_server/models/db.js
@@ -4,7 +4,7 @@ const dbURI = 'mongodb://matiasrantanen:testi123@ds233323.mlab.com:33323/nhlnews
 
 mongoose.connect(dbURI, {useNewUrlParser: true});
 
-//Console messages
+//Console messages 
 mongoose.connection.on('connected', function(){
     console.log(`Mongoose connected to ${dbURI}`);
 });
